3c19a9e06ff4031956c8468e9ca75b5637a94641
LucasGLGoncalves <lucas.gabrielsglgl@gmail.com>
2026-05-17 18:57:15 -0400

    comentarios e refinamento do script seq

---
+++ capitulo2/dialog.sh
@@ -0,0 +1,32 @@
+# !/bin/bash
+#
+# dialog.sh - script de estudo do capitulo 2 sobre o comando dialog
+#
+# Repositório   : https://github.com/LucasGLGoncalves/Shell-Script-Profissional
+# Autor         : Lucas Gonçalves
+# Manutenção    : Lucas Gonçalves
+#
+# --------------------------------------------------------------------------------
+# Este é um script simples
+# para representar o estudo
+# do capitulo 2 do livro
+#
+# Exemplos:
+# em breve...
+#
+# Explicação em breve
+# --------------------------------------------------------------------------------
+#
+#
+# Histórico:
+#
+# v0.1  2026-05-17, Lucas Gonçalves:
+#       - Versão inicial, apenas criação e esboço
+#
+#
+# Licença: GPL.
+#
+dialog \
+	--title "minha telinha" \
+	--msgbox "Esta é minha mensagem" \
+	0 0
--- capitulo2/seq.sh
+++ capitulo2/seq.sh
@@ -18,5 +18,47 @@
 #
 # Uso em scripts: for i in $(seq5 10); do comandos ; done
 #
-o=+ a=1 z=${1:-1}; [ "$2" ] && { a=$1; z=$2; } ; [ $a -gt $z ] && o=-
-while [ $a -ne $z ]; do echo $a ; eval "a=\$((a $o 1))"; done; echo $a
+
+### Inicialização das variáveis
+o=+ 		# Operação a ser feita. Pode ser + ou -
+a=1		# Valor padrão de início de contagem
+z=${1:-1}	# Valor do final da contagem recebido em $1 (padrão é 1)
+
+# A variável 'a' é o número de início e a variável 'z' é o final
+#
+# O código anterior serve para quando o usuário passar apenas um valor na
+# linha de comando, que seria o valor do *final* da contagem. Por isso
+# a=1 e z=$1
+#
+# Caso o programa seja chamado sem parâmetros, 'z' também é definido
+# como 1. Como a=1 e z=1, o programa retorna apenas o número 1.
+
+[ "$2" ] && {
+	
+	# Foram passados 2 parâmetros, o $1 é o início e o $2 é o fim.
+	a=$1
+       	z=$2
+}
+
+###
+# Se o número inicial ($a) for maior que o número final ($z),
+# faremos uma contagem regressiva, por isso a operação é definida
+# como subtração.
+#
+[ $a -gt $z ] && o=-
+
+###
+# Loop da contagem (progressiva ou regressiva)
+#
+# A cada volta, adiciona ou subtrai 1 do valor inicial,
+# até que ele se iguale ao final.
+#
+# O eval executa o resultado da expansão de variáveis.
+# Supondo o='+' e a=3, o eval executará o comando a=$((3+1)).
+#
+while [ $a -ne $z ]; do
+	echo $a			# mostra a contagem atual
+	eval "a=\$((a $o 1))"	# efetua a operação (+1 ou -1)
+done
+
+echo $a				# mostra o último número
