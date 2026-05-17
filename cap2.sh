51e89c6c5fc6bf51d82df6e6640dba2eb671b3b7
LucasGLGoncalves <lucas.gabrielsglgl@gmail.com>
2026-05-17 16:16:44 -0400

    commit inicial

---
+++ cap2.sh
@@ -0,0 +1,26 @@
+# !/bin/bash
+#
+# cap2.sh - scripts de estudo do capitulo 2
+#
+# Repositório	: https://github.com/LucasGLGoncalves/Shell-Script-Profissional
+# Autor		: Lucas Gonçalves
+# Manutenção	: Lucas Gonçalves
+#
+# --------------------------------------------------------------------------------
+# Este é um script simples
+# para representar o estudo
+# do capitulo 2 do livro
+#
+# Exemplos:
+# em breve...
+#
+# Explicação em breve
+# --------------------------------------------------------------------------------
+#
+#
+# Histórico:
+#
+# v0.1	2026-05-17, Lucas Gonçalves:
+# 	- Versão inicial, apenas criação e esboço
+
+echo "Este é um comando"
